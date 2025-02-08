a4829408ead77341f1b432ac2e32b67ef35bea2e
Roberto Argueta <roberto_argueta.1998@hotmail.com>
2025-02-07 18:51:44 -0600

    deploy 3

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,9 @@ import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  base: '/Perseverus-Page/', // Ajusta esto según el nombre de tu repositorio
   plugins: [
     react(),
     tailwindcss(),
   ],
+  base: '/Perseverus-Page/', // Ajusta esto según el nombre de tu repositorio
 });
